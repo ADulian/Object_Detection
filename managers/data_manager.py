c93b424e037feaa361d80ccc916250679cbe4291
ADulian <duli95@gmail.com>
2023-06-20 09:52:40 +0100

    Basic Logging

--- managers/data_manager.py
+++ managers/data_manager.py
@@ -1,1 +1,22 @@
+"""
+ToDo:
+    - Loading COCO dataset
+
+"""
+
+import logging
+
+log = logging.getLogger("lightning")
+
+# --------------------------------------------------------------------------------
+class DataManager:
+    # --------------------------------------------------------------------------------
+    def __init__(self):
+
+
+
+        log.info("Data Manager Initialized")
+
+
+
 
---
+++ train.py
@@ -0,0 +1,22 @@
+import logging
+
+from managers.data_manager import DataManager
+
+# Setup Logging
+log = logging.getLogger("lightning")
+log.setLevel(logging.INFO)
+
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+log.addHandler(ch)
+
+# --------------------------------------------------------------------------------
+def main():
+    """ Main
+    """
+
+    dm = DataManager()
+
+# --------------------------------------------------------------------------------
+if __name__ == '__main__':
+    main()
