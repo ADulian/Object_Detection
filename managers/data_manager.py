ce2965830805f5a859f3bcc5f760fa7904ad6360
ADulian <duli95@gmail.com>
2023-06-20 21:41:28 +0100

    Added coco dataset class

--- managers/data_manager.py
+++ managers/data_manager.py
@@ -1,6 +1,8 @@
 """
 ToDo:
     - Loading COCO dataset
+    - Incorporating Lightning Data Module
+    - Setup Train/Val/Test Loaders
 
 """
 
@@ -10,9 +12,14 @@ log = logging.getLogger("lightning")
 
 # --------------------------------------------------------------------------------
 class DataManager:
+    """Data Manager class
+
+    General management of datasets
+    """
     # --------------------------------------------------------------------------------
     def __init__(self):
-
+        """Initialize Data Manager
+        """
 
 
         log.info("Data Manager Initialized")
