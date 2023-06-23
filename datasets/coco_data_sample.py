818ebd42cb7eddb643cae1eb4eddb4e8b44063bd
ADulian <duli95@gmail.com>
2023-06-23 09:07:20 +0100

    Added Initializing Coco samples

--- datasets/coco_data_sample.py
+++ datasets/coco_data_sample.py
@@ -1,6 +1,45 @@
+import numpy as np
+
 # --------------------------------------------------------------------------------
 class CocoDataSample:
+    """Wrapper for Coco Samples
+    """
 
     # --------------------------------------------------------------------------------
-    def __init__(self):
-        ...
+    def __init__(self,
+                 img_data: dict,
+                 annotation_data: list[dict]):
+
+        """Initialize Coco Data Sample
+
+        Args:
+            img_data: (dict): Img related data
+            annotation_data: (list[dict]): A list of annotations per sample
+
+        """
+
+        # Image Data
+        self.img_id = img_data["id"]
+        self.img_file_name = img_data["file_name"]
+        self.img_height = img_data["height"]
+        self.img_width = img_data["width"]
+
+        # Annotations Data
+        self.annotations = [CocoAnnotation(annotation=annotation) for annotation in annotation_data]
+
+# --------------------------------------------------------------------------------
+class CocoAnnotation:
+    """Wrapper for a single annotation
+    """
+
+    # --------------------------------------------------------------------------------
+    def __init__(self, annotation: dict):
+        """Initialize Coco Annotation
+        """
+
+        self.id = annotation["id"]
+        self.image_id = annotation["image_id"]
+        self.is_crowd = annotation["iscrowd"]
+        self.area = annotation["area"]
+        self.category = annotation["category_id"]
+        self.bbox = np.array(annotation["bbox"], dtype=float)
--- datasets/coco_dataset.py
+++ datasets/coco_dataset.py
@@ -17,18 +17,18 @@ class CocoDataset:
                  imgs_path: Path,
                  annotations_file: (Path | None) = None):
 
-        self._data_samples = self._load_data(imgs_path=imgs_path, annotations_file=annotations_file)
+        self._imgs_path = imgs_path
+        self._data_samples = self._load_data(annotations_file=annotations_file)
+
         log.info("Coco Dataset Initialized")
 
     # --------------------------------------------------------------------------------
-    def _load_data(self,
-                   imgs_path: Path,
-                   annotations_file: (Path | None) = None) -> list[CocoDataSample]:
+    @staticmethod
+    def _load_data(annotations_file: (Path | None) = None) -> list[CocoDataSample]:
 
         """Load img and annotation data for coco
 
         Args:
-            imgs_path: (Path): Root path to images
             annotations_file: (Path | None): Root path to annotations if file exists
 
         Returns:
@@ -42,9 +42,6 @@ class CocoDataset:
 
         imgs_data_json, annotations_data_json = data_json["images"], data_json["annotations"]
 
-        # Samples
-        data_samples = []
-
         # Img + Annotations
         imgs_data = {}
         annotations_data = defaultdict(list)
@@ -56,6 +53,11 @@ class CocoDataset:
         # Populate annotations
         for annotation_data in annotations_data_json:
             annotations_data[annotation_data["image_id"]].append(annotation_data)
+
+        # Initialize data samples
+        data_samples = [CocoDataSample(img_data=img_data,
+                                       annotation_data=annotations_data[img_key])
+                        for img_key, img_data in imgs_data.items()]
 
         return data_samples
 
