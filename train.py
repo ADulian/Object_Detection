50fe78ca7fb27e75f4452942103f1caada1b7be6
ADulian <duli95@gmail.com>
2023-06-30 08:56:34 +0100

    Added args options to dm

--- train.py
+++ train.py
@@ -16,7 +16,13 @@ def main():
     args = parse_args()
 
     # Init Data Manager
-    dm = DataManager(coco_root_path=args.coco_path)
+    dm = DataManager(coco_root_path=args.coco_path,
+                     train_batch_size=args.train_bs,
+                     val_batch_size=args.val_bs,
+                     test_batch_size=args.test_bs,
+                     shuffle=args.shuffle,
+                     pin_memory=args.pin_memory,
+                     num_workers=args.num_workers)
     dm.setup()
 
 # --------------------------------------------------------------------------------
