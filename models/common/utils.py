88fe10218537447b334907166dedd0dc11105ff2
ADulian <duli95@gmail.com>
2023-07-11 09:54:13 +0100

    Added functionality to compute padding

--- models/common/utils.py
+++ models/common/utils.py
@@ -22,3 +22,22 @@ def get_layer(layer: str) -> Callable:
             return getattr(module, layer)
 
     raise ValueError(f"{layer} not found in any of the modules")
+
+# --------------------------------------------------------------------------------
+def auto_pad(kernel_size: int,
+             padding: (int | None) = None) -> int:
+    """Auto padding based on kernel size
+
+    Args:
+        kernel_size: (int): Kernel Size
+        padding: (int | None): Padding, if None then the value will be computed from kernel
+
+    Returns:
+        int: Padding value
+
+    """
+
+    if padding is None:
+        return kernel_size // 2
+    else:
+        return padding
