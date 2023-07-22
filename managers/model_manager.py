96892d71214d6eac1a0006dbb742ab134912eb46
ADulian <duli95@gmail.com>
2023-07-22 17:24:02 +0100

    Added basic model manager

---
+++ managers/model_manager.py
@@ -0,0 +1,44 @@
+from typing import Type
+
+from models.yolo_v1.model import YoloV1
+from models.yolo_v1.gt_generator import YoloV1GTGenerator
+
+# --------------------------------------------------------------------------------
+class ModelManager:
+    """Model Manager Class
+    """
+
+    # --------------------------------------------------------------------------------
+    def __init__(self,
+                 num_classes: int):
+        """Initialise Model Manager
+
+        Args:
+            num_classes: (int): Number of classes for Yolo prediction
+        """
+
+        self.model, self.gt_generator = self._init_model(num_classes=num_classes)
+
+    # --------------------------------------------------------------------------------
+    def _init_model(self,
+                    num_classes: int) -> tuple[YoloV1, Type[YoloV1GTGenerator]]:
+        """Initialize model and get reference to ground truth generator
+        Args:
+            num_classes: (int): Number of classes for Yolo prediction
+        """
+
+        model = YoloV1(num_classes=num_classes)
+
+        return model, YoloV1GTGenerator
+
+    # --------------------------------------------------------------------------------
+    def save_weights(self):
+        """Save model's weights
+        """
+        ...
+
+    # --------------------------------------------------------------------------------
+    def load_weights(self):
+        """Load model's weights
+        """
+        ...
