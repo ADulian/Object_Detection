0fccf327569bfc8a1b16d283e72db918990fc0ea
ADulian <duli95@gmail.com>
2023-07-22 15:48:08 +0100

    Updated bounding box objects

--- utils/types.py
+++ utils/types.py
@@ -3,6 +3,7 @@ from enum import Enum
 from typing import TypedDict
 from PIL import Image, ImageDraw
 
+# --------------------------------------------------------------------------------
 class BBoxFormat(Enum):
     """Bounding Box Format
 
@@ -32,7 +33,10 @@ class BBoxFormat(Enum):
     |>>>>>^
     """
 
+# --------------------------------------------------------------------------------
 class BBoxParts(TypedDict):
+    """Parts of bounding box
+    """
     top_left: np.ndarray
     bottom_right: np.ndarray
     width_height: np.ndarray
@@ -65,48 +69,34 @@ class BBox:
 
     # --------------------------------------------------------------------------------
     def _set_bbox_parts(self) -> BBoxParts:
-        """Set all parts of bounding box
+        """Set all parts of bounding box and update values
 
         Returns:
             BBoxParts: A dictionary of bounding box parts
         """
 
-        top_left = np.array([self.top_x, self.top_y], dtype=float)
-        bottom_right = np.array([self.bottom_x, self.bottom_y], dtype=float)
-        width, height = self.width, self.height
-        mid_x_y = np.array([self.mid_x, self.mid_y], dtype=float)
-
         if self.bbox_format == BBoxFormat.XYXY:
-            top_left = self.bbox[:2]
-            bottom_right = self.bbox[2:]
-            width, height = bottom_right[0] - top_left[0], top_left[1] - bottom_right[1]
-            mid_x_y = np.array([bottom_right[0] - (width / 2), bottom_right[1] - (height / 2)], dtype=float)
+            self.top_x, self.top_y = self.bbox[0], self.bbox[1]
+            self.bottom_x, self.bottom_y = self.bbox[2], self.bbox[3]
+            self.width, self.height = self.bottom_x - self.top_x, self.bottom_y - self.top_y
+            self.mid_x, self.mid_y = self.bottom_x - (self.width / 2), self.bottom_x - (self.height / 2)
 
         elif self.bbox_format == BBoxFormat.XYWH:
-            top_left = self.bbox[:2]
-            width, height = self.bbox[2], self.bbox[3]
-            bottom_right = np.array([top_left[0] + width, top_left[1] + height], dtype=float)
-            mid_x_y = np.array([bottom_right[0] - (width / 2), bottom_right[1] - (height / 2)], dtype=float)
+            self.top_x, self.top_y = self.bbox[0], self.bbox[1]
+            self.width, self.height = self.bbox[2], self.bbox[3]
+            self.bottom_x, self.bottom_y = self.top_x + self.width, self.top_y + self.height
+            self.mid_x, self.mid_y = self.bottom_x - (self.width / 2), self.bottom_x - (self.height / 2)
 
         elif self.bbox_format == BBoxFormat.MID_X_MID_Y_WH:
-            mid_x_y = self.bbox[:2]
-            width, height = self.bbox[2], self.bbox[3]
-            top_left = np.array([mid_x_y[0] - (width / 2), mid_x_y[1] - (height / 2)], dtype=float)
-            bottom_right = top_left = np.array([mid_x_y[0] + (width / 2), mid_x_y[1] + (height / 2)], dtype=float)
+            self.mid_x, self.mid_y = self.bbox[0], self.bbox[1]
+            self.width, self.height = self.bbox[2], self.bbox[3]
+            self.top_x, self.top_y = self.mid_x - (self.width / 2), self.mid_y - (self.height / 2)
+            self.bottom_x, self.bottom_y = self.top_x + self.width, self.top_y + self.height
 
-        bbox_parts = {"top_left" : top_left,
-                      "bottom_right": bottom_right,
-                      "width_height": np.array([width, height], dtype=float),
-                      "mid_x_y": mid_x_y}
-
-        self.width = width
-        self.height = height
-        self.top_x = top_left[0]
-        self.top_y = top_left[1]
-        self.bottom_x = bottom_right[0]
-        self.bottom_y = bottom_right[1]
-        self.mid_x = mid_x_y[0]
-        self.mid_y = mid_x_y[1]
+        bbox_parts = {"top_left" : np.array([self.top_x, self.top_y], dtype=float),
+                      "bottom_right": np.array([self.bottom_x, self.bottom_y], dtype=float),
+                      "width_height": np.array([self.width, self.height], dtype=float),
+                      "mid_x_y": np.array([self.mid_x, self.mid_y], dtype=float)}
 
         return bbox_parts
 
@@ -136,7 +126,15 @@ class BBox:
 
     # --------------------------------------------------------------------------------
     def clamp_bbox(self,
-                   max_value):
+                   max_width: int,
+                   max_height: int):
+        """Clamp bounding box with (0,0) - (max_width, max_height)
+
+        Args:
+            max_width: (int): Max width boundary
+            max_height: (int): Max height boundary
+
+        """
 
         # Get bounding box in correct formatS
         bbox = self.get_bbox(bbox_format=BBoxFormat.XYXY)
@@ -146,8 +144,8 @@ class BBox:
         bbox[1] = max(0, bbox[1])
 
         # Clamp xy2
-        bbox[2] = max(max_value, bbox[2])
-        bbox[3] = max(max_value, bbox[2])
+        bbox[2] = max(max_width, bbox[2])
+        bbox[3] = max(max_height, bbox[2])
 
         # Update Bounding box
         self.update_bbox(bbox=bbox, bbox_format=BBoxFormat.XYXY)
@@ -156,18 +154,33 @@ class BBox:
     def update_bbox(self,
                     bbox: np.ndarray,
                     bbox_format: BBoxFormat):
+        """Update bounding box information
+
+        Args:
+            bbox: (np.ndarray): A new bounding box
+            bbox_format: (BBoxFormat): A format of new bounding box
+
+        """
 
         self.bbox = bbox
         self.bbox_format = bbox_format
         self.bbox_parts = self._set_bbox_parts()
 
     def draw_box(self, img: Image.Image):
+        """Draw a bounding box on the PIL Image
+
+        Args:
+            img: (Image.Image): Image object
+
+        """
+
+        if not isinstance(img, Image.Image):
+            raise ValueError(f"Can only draw on PIL image, given type: {type(img)}")
+
         draw = ImageDraw.Draw(img)
 
         # Define the coordinates of the rectangle (left, top, right, bottom)
-        bbox = self.get_bbox(bbox_format=BBoxFormat.XYXY).astype(int)
+        bbox = self.get_bbox(bbox_format=BBoxFormat.XYXY)
 
         # Draw the rectangle on the image
-        draw.rectangle(bbox, outline="red", width=100)
-        img.show()
-        return img
+        draw.rectangle(tuple(bbox), outline="red")
