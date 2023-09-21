8766f56ee8cd99417731f9090aa94acc08962b89
ADulian <duli95@gmail.com>
2023-09-21 09:00:52 +0100

    Finished writing basic tests for unnormalized bboxs

--- metrics/tests/test_iou.py
+++ metrics/tests/test_iou.py
@@ -9,38 +9,58 @@ class Test_IoU:
     """
 
     # BBoxs
-    BBOX_1_UNNORMALIZED = BBox(bbox=np.array([350, 400, 550, 700], dtype=np.float),
-                               bbox_format=BBoxFormat.XYXY)
+    BBOX_1 = BBox(bbox=np.array([350, 400, 550, 700], dtype=np.float),
+                  bbox_format=BBoxFormat.XYXY)
 
-    BBOX_2_UNNORMALIZED = BBox(bbox=np.array([450, 550, 600, 950], dtype=np.float),
-                               bbox_format=BBoxFormat.XYXY)
+    BBOX_2 = BBox(bbox=np.array([450, 550, 600, 950], dtype=np.float),
+                  bbox_format=BBoxFormat.XYXY)
 
     # True IoUs
-    IOU_UNNORMALIZED = 0.1429
-
+    IOU = 0.1429
 
     # --------------------------------------------------------------------------------
-    def test_xyxy(self):
-        """Test IoU on XYXY bbox
-        """
+    def get_iou(self,
+                bbox_format: BBoxFormat):
 
-        # Get XYXY bboxs
-        bbox_1 = self.BBOX_1_UNNORMALIZED.get_bbox(bbox_format=BBoxFormat.XYXY)
-        bbox_2 = self.BBOX_2_UNNORMALIZED.get_bbox(bbox_format=BBoxFormat.XYXY)
+        """Compute IoU between unnormalized bboxes
+        """
+        bbox_1 = self.BBOX_1.get_bbox(bbox_format=bbox_format)
+        bbox_2 = self.BBOX_2.get_bbox(bbox_format=bbox_format)
 
         # Get IoU between bboxes
-        _iou = iou(bbox_1=bbox_1, bbox_2=bbox_2, bbox_format=BBoxFormat.XYXY).item()
+        _iou = iou(bbox_1=bbox_1, bbox_2=bbox_2, bbox_format=bbox_format).item()
 
         # Round to 4 decimal places
         _iou = round(_iou, 4)
 
-        assert self.IOU_UNNORMALIZED == _iou
+        return _iou
+
+    # --------------------------------------------------------------------------------
+    def test_xyxy(self):
+        """Test IoU on XYXY bboxs
+        """
+
+        # Compute XYXY IoU
+        _iou = self.get_iou(bbox_format=BBoxFormat.XYXY)
+
+        assert self.IOU == _iou
 
     # --------------------------------------------------------------------------------
     def test_xywh(self):
-        ...
+        """Test IoU on XYWH bboxs
+        """
+
+        # Compute XYWH IoU
+        _iou = self.get_iou(bbox_format=BBoxFormat.XYWH)
+
+        assert self.IOU == _iou
 
     # --------------------------------------------------------------------------------
     def test_mid_x_mid_y_wh(self):
-        ...
+        """Test IoU on Mid-X Mid-Y WH bboxs
+        """
 
+        # Compute XYWH IoU
+        _iou = self.get_iou(bbox_format=BBoxFormat.MID_X_MID_Y_WH)
+
+        assert self.IOU == _iou
