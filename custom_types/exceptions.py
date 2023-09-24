f990d2494d2f3e92a6ee05f9b501d21d3ea58d71
ADulian <duli95@gmail.com>
2023-09-24 12:08:32 +0100

    Added custom exception to handle incorrect lenght of tensors dimensions

---
+++ custom_types/exceptions.py
@@ -0,0 +1,20 @@
+
+# --------------------------------------------------------------------------------
+class DimensionLengthError(Exception):
+    """Exception for handling incorrect length of dimensions in a tensor
+    """
+
+    # --------------------------------------------------------------------------------
+    def __init__(self,
+                 dim_length: int,
+                 target_dim_length: int,
+                 add_msg: str = ""):
+        """
+
+        Args:
+            dim_length: (int): Actual length of dimensions
+            target_dim_length: (int): Expected length of dimensions
+            add_msg: (str): Optional, additional message
+        """
+        super().__init__(f"Incorrect length of dimensions! Expected: {target_dim_length}, Actual: {dim_length} "
+                         + add_msg)
