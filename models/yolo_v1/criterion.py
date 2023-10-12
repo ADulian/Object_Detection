f27ea10dc8d6d9f8f5b173d02c7f50a9368b914f
ADulian <duli95@gmail.com>
2023-10-12 21:08:03 +0100

    Tested 2 boxes prediction and selection

--- models/yolo_v1/criterion.py
+++ models/yolo_v1/criterion.py
@@ -229,9 +229,10 @@ class YoloV1Criterion:
         """
 
         # Sanity Check
-        y = torch.ones(1, 7, 7, 6) * 2
-        y_hat = torch.ones(1, 7, 7, 90) * 4
-        # y_hat[..., :5]
+        y = torch.ones(1, 7, 7, 6) * 4
+        y_hat = torch.ones(1, 7, 7, 90)
+        y_hat[..., :5] *= 2 # True boxes
+        y_hat[..., 5:10] *= 4
         assert y.shape[:-1] == y_hat.shape[:-1], f"Something went wrong, \nshape-> y: {y.shape}, y_hat: {y_hat.shape}"
 
         # Sigmoid Y_Hat
@@ -242,23 +243,23 @@ class YoloV1Criterion:
         n_hat = y_hat.shape[-1]
 
         # Compute and Compare IoUs of predicted boxes
-        # y_hat_bboxs_1_indices, y_hat_bboxs_2_indices = self._compare_ious(y, y_hat)
+        y_hat_bboxs_1_indices, y_hat_bboxs_2_indices = self._compare_ious(y, y_hat)
 
         # Reshape
         y = y.view(batch_size * s * s, n)
         y_hat = y_hat.view(batch_size * s * s, n_hat)
 
         # BBox + Confidence(Object) (mid_x, mid_y, w, h, p)
-        # y_hat_bboxs = torch.zeros(*y[:, :5].shape, device=y.device)
+        y_hat_bboxs = torch.zeros(*y[:, :5].shape, device=y.device)
 
-        # y_hat_bboxs[y_hat_bboxs_1_indices] = y_hat[:, :5][y_hat_bboxs_1_indices]
-        # y_hat_bboxs[y_hat_bboxs_2_indices] = y_hat[:, 5:10][y_hat_bboxs_2_indices]
+        y_hat_bboxs[y_hat_bboxs_1_indices] = y_hat[:, :5][y_hat_bboxs_1_indices]
+        y_hat_bboxs[y_hat_bboxs_2_indices] = y_hat[:, 5:10][y_hat_bboxs_2_indices]
 
         # Mask Obj
         mask_obj = (y[:, -2] == 1.0).unsqueeze(-1)
 
         # --- Compute Loss terms
-        y_hat_bboxs = y_hat[..., :5]
+        # y_hat_bboxs = y_hat[..., :5]
         x_y_loss = self._compute_x_y_loss(y=y, y_hat_bboxs=y_hat_bboxs, mask_obj=mask_obj)
         w_h_loss = self._compute_w_h_loss(y=y, y_hat_bboxs=y_hat_bboxs, mask_obj=mask_obj)
         # c_obj_loss, c_no_obj_loss = self._compute_confidence_loss(y=y, y_hat_bboxs=y_hat_bboxs, mask_obj=mask_obj)
